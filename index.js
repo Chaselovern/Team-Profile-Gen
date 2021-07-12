58be42764269a585c4fe8c6eb77b068bdce0295f
Chase Lovern <Chaselovern@gmail.com>
2021-07-11 22:28:17 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const inquirer = require("inquirer");
+const employee = require("./lib/Employee");
+const engineer = require("./lib/Engineer");
+const intern = require("./lib/Intern");
+const manager = require("./lib/Manager");
+const fs = require("fs");
+
+
+fucntion htmlPage(){
+    const html =  `<!DOCTYPE html>`
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, inital-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
+        <title>Team Profile Generator</title>
+    </head>
+    <body>
+        <nav class = "navbar navbar-dark bg-dark mb-5">
+            <span class = "navbar-brand mb-0 h1 w-100 text-center">Team Profile Generator</span>
+        </nav>
+        <div class = "container"></div>
+    </body>
+}
